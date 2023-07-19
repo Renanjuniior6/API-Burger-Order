5ab967bce2f9cb3818491c36ca961cf22f47c110
Renanjuniior6 <renanjrr679@gmail.com>
2023-07-19 14:21:14 -0300

    port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.port || 3001 
+const port = process.env.PORT || 3001 
 const uuid = require('uuid')
 
 app.use(express.json())
